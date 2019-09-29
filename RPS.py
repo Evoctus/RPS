5064e777ffdfe5ee2c695738a0251095833d606e
Evoctus <55909399+Evoctus@users.noreply.github.com>
2019-09-28 21:01:12 -0700

    Create RPS.py

---
+++ RPS.py
@@ -0,0 +1,46 @@
+from random import randint
+
+t = ("Rock", "Paper", "Scissors")
+
+d = ("Yes", "No")
+
+computer = t[randint (0,2)]
+
+player = False
+
+while player == False:
+    player = input ("Rock, Paper, Scissors?")
+    if player == computer:
+        print ("Tie")
+    elif player == "Rock":
+        if computer == "Paper":
+            print ("YOU LOSE NERD! Paper covers Rock up")
+        else:
+            print ("You win! Rock crushes Scissors")
+    elif player == "Paper":
+        if computer == "Scissors":
+            print ("YOU LOSE NERD! Scissors cuts right through Paper")
+        else:
+            print ("You win! Paper covers Rock up")
+    elif player == "Scissors":
+        if computer == "Rock":
+            print ("YOU LOSE NERD! Rock crushes Scissors")
+        else:
+            print ("You win! Scissors cuts right through Paper")
+    else:
+        print ("I cannot understand you! Please ensure that you have proper capitalization!")
+    
+player = True
+
+while player == True:
+    player = input ("Continue? Yes or No?")
+    if player == "Yes":
+        player = False
+    elif player == "No":
+        break
+    else:
+        print ("I cannot understand you! Please ensure that you have proper capitalization!")
+    
+   
+            
+    
